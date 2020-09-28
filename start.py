5d30305767920f327ef251755b1e7a64ea5fda56
rizka <rizkaputriocta11@gmail.com>
2020-09-28 22:15:35 +0700

    fungsi phyton

---
+++ start.py
@@ -0,0 +1,28 @@
+#KONSTRUKSI DASAR PHYTON
+#SEQUENTIAL
+print ("hello world")
+print ("by Rizka")
+print ("11 oktober 2001")
+print ("--"*3)
+print ("muhammad Nazriel")
+print ("8 mei 2008")
+print ("--"*3)
+
+# percabangan : Eksekusi terpilih
+ingin_pergi = False
+if ingin_pergi:
+    print ('sekarang saja')
+else:
+    print ('nanti saja')
+
+#pengulangan
+jumlahh_ikan = 4
+
+for index_ikan in range (1, jumlahh_ikan+1): #jumlah pengulangan = 5-1 = 4
+    print (f'pyek pyek #{index_ikan }')
+
+
+
+
+
+
